5d97dbfb3a9adf1e317b2e2efc3cbcf159156c13
David Piper <david.piper@mailfence.com>
2019-04-04 13:09:35 -0500

    feat: finish making basic timer
    This completes the timer, as initially designed and expected.
    It was done completely using TDD, thus everything is fully
    tested.
    
    Features include:
    
    - simple stop/start interface for timing code
    - nicely formatted string output, that is it looks like
      how a person would say how much time has passed:
       i.e. "1 minute and 30 seconds"
    
    Signed-off-by: David Piper <david.piper@mailfence.com>

--- src/__tests__/timer.test.ts
+++ src/__tests__/timer.test.ts
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import Timer from '../timer';
 
 // tslint:disable: no-magic-numbers
@@ -9,7 +10,27 @@ describe('timer tests', () => {
 
     // since all we did was start and stop the timer
     // that should not take very long...
-    expect(result.seconds).toBeLessThan(10);
+    expect(result.seconds).toBeLessThan(1);
     expect(result.minutes).toBe(0);
+  });
+
+  test('works with a timeout of 50 ms', async () => {
+    const timer = new Timer();
+    const timeout = promisify(setTimeout);
+    await timeout(50);
+    const result = timer.stop();
+
+    expect(result.milliseconds).toBeGreaterThanOrEqual(50);
+    expect(result.milliseconds).toBeLessThan(300);
+  });
+
+  test('works with a timeout of a couple of seconds', async () => {
+    const timer = new Timer();
+    const timeout = promisify(setTimeout);
+    await timeout(1000 * 2);
+    const result = timer.stop();
+
+    expect(result.seconds).toBeGreaterThanOrEqual(2);
+    expect(result.seconds).toBeLessThanOrEqual(2.5);
   });
 });
